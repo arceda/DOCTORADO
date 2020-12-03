a9cc502ea3905ebf19fb8cdac836d6a6c66f06f0
vicente <enriquefirst@gmail.com>
2020-12-02 21:27:36 -0500

    pso MSA

---
+++ ia/src/thesis/pe_seed.py
@@ -0,0 +1,205 @@
+import numpy as np
+import random
+
+def split(word): 
+    return [char for char in word]  
+
+# compute overlap complexity of two space seeds
+def slowOC(s_1, s_2):
+    s_1 = split(s_1)
+    s_2 = split(s_2)
+
+    t_1 = np.zeros( len(s_1) + 2*len(s_2) - 2)
+    t_1[len(s_2) - 1:len(s_2) - 1 + len(s_1)] = s_1
+
+    sigma = []
+    result = []
+
+    for i in range( len(s_1) + len(s_2) - 1 ):        
+        t_2 = np.zeros( len(s_1) + 2*len(s_2) - 2)       
+        t_2[i:len(s_2) + i] = s_2
+
+        matches = np.logical_and(t_1, t_2)
+        sigma_tmp = (matches == True).sum()
+        sigma.append( sigma_tmp )
+        result.append( 2**sigma_tmp )     
+
+    return np.sum( result ), sigma
+
+
+def quicklyOC(s_1, s_2):
+    wight_1 = np.where( np.array(s_1) == 1)
+    wight_2 = np.where( np.array(s_2) == 1)
+    sigma = np.zeros( ( len(s_1) +  len(s_2) - 1 ) )
+    #print(sigma)
+
+    print(wight_1, wight_2)
+    for i in wight_1[0]:
+        for j in wight_2[0]:
+            print(i, j, i + j)
+            sigma[ i + j ] += 1
+
+    #print(sigma) 
+
+
+def overlap_complexity(S):
+    #quicklyOC( S[0], S[1] )
+    total_oc = 0 # sum overlap complexity
+    for i in range ( len(S) - 1):
+        oc, sigma =  slowOC( S[i], S[i + 1] )
+        total_oc += oc
+
+    return total_oc
+    #print(total_oc)
+    
+def count_occurrence_in_seed(S, model):    
+    acc = 0
+    for seed in S:        
+        acc += seed.count(model)
+        #print(S_str, model_str, S_str.count(model_str))
+    return acc
+
+# find the sibstring in the table of frequencies
+def find_position(table, substring):
+    if substring == '0011' or substring == '1011' or substring == '0111':
+        return 0
+    elif substring == '0101' or substring == '1101' or substring == '0111':
+        return 1
+    elif substring == '0110' or substring == '1110' or substring == '0111':
+        return 1
+    elif substring == '0101' or substring == '1101' or substring == '0111':
+        return 1
+    elif substring == '0101' or substring == '1101' or substring == '0111':
+        return 1
+    elif substring == '0101' or substring == '1101' or substring == '0111':
+        return 1
+
+def build_frequency_pattern_table(S):    
+    table = []
+    patterns = []
+    patterns.append( ['0011','1011','0111'] ) #0
+    patterns.append( ['0101','1101','0111'] ) #1
+    patterns.append( ['0110','1110','0111'] ) #2
+    patterns.append( ['1001','1101','1011'] ) #3
+    patterns.append( ['1010','1110','1011'] ) #4
+    patterns.append( ['1100','1101','1110'] ) #5
+    patterns.append( ['0111'] ) #6
+    patterns.append( ['1011'] ) #7
+    patterns.append( ['1101'] ) #8
+    patterns.append( ['1110'] ) #9
+
+    for patt in patterns:
+        occurrence = 0
+        for model in patt:
+            occurrence += count_occurrence_in_seed(S, model)
+
+        table.append(occurrence)
+
+    return table, patterns
+
+#S = [ "1011", "100101" ]
+#print(overlap_complexity(S))
+
+#S = ["1011010110101", "1110111000101", "1001110101101"]
+#table = build_frequency_pattern_table(S)
+#print(table)
+
+################################################################################
+# PARAMETERS
+################################################################################
+#penguins_number = 25
+penguins_number = 5
+groups_number = 10
+iterations = 20
+oxigen_reserve = 1
+
+seed_length = 11
+seed_weight = 7
+
+################################################################################
+
+
+################################################################################
+# CREATE POPULATION
+################################################################################
+population = []
+groups = []
+
+# create random population of penguin (seed)
+for i in range(penguins_number*groups_number):
+    penguin = np.zeros( seed_length ).astype(int)
+    indexes = list(range( seed_length ))
+    random.shuffle(indexes)
+    
+    for idx in range(seed_weight):
+        penguin[ indexes[idx] ] = 1
+
+    penguin = ''.join( list( map(str, penguin) ) )
+    population.append( penguin )
+
+population = np.array( population )
+oxigen = np.zeros( (population.shape[0], 1) ).astype(int) + 2
+population = np.hstack( (population.reshape(  population.shape[0],1 ), oxigen ) )
+
+#print(population)
+
+for i in range( groups_number ):
+    groups.append( population[ i*penguins_number : (i+1)*penguins_number ] )
+
+# create the frequency table
+table_frequency, patterns = build_frequency_pattern_table( population[:, 0] )
+min_pattern = np.argmin(table_frequency)
+#print( patterns[min_pattern][0] )
+
+#print( population )
+
+#for gr in groups:
+#    print(gr)
+
+################################################################################
+
+
+################################################################################
+# PENGUIN SEARCH
+################################################################################
+best_oc = 0
+
+for iter in range(iterations):
+    print("\n\nITERATION: ", iter, "***************************************")
+
+    #print(groups[0])   
+    #print(population)   
+
+    oc_tmp = []
+    for gr in groups:
+        oc_tmp.append( overlap_complexity( gr[:, 0] ) )
+    
+    best_oc = np.min( oc_tmp )
+    print( "OC of each group:", oc_tmp )
+    
+    for gr in groups:
+        #print("\nGROUP ********************************")
+        table_frequency, patterns = build_frequency_pattern_table( gr[:, 0] )
+        min_pattern = np.argmin(table_frequency)
+
+        for penguin in gr:
+            #print("\nPENGUIN:", penguin[0])
+            seed = penguin[0]            
+            while int(penguin[1]) > 0:
+                #improve seed             
+                pos = random.randint( 0, len(seed)-5 ) # -5 porque extraeremos 4 substring
+                
+                # reemplazamos en el seed por el patron con menor frecuencia en S            
+                old_seed = seed         
+                seed = seed[0:pos] + patterns[min_pattern][0] + seed[pos + 4:len(seed)]
+                #print("seed: ", old_seed, "pos:", pos, "new seed:", seed)
+
+                penguin[0] = seed
+                penguin[1] =  str(int(penguin[1]) - 1)
+            
+    
+         
+    
+
+
+
---
+++ ia/src/thesis/pso.py
@@ -0,0 +1,199 @@
+import numpy as np
+from Bio import SeqIO
+import math
+import random
+import scipy.stats
+import pandas as pd
+import sys
+import numpy as np
+from Bio import SeqIO
+import re
+import os
+import glob
+np.set_printoptions(precision=4, suppress=True)
+random.seed(1)
+from numpy.random import seed
+seed(1)
+
+######## global variables ##################################### 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# particle format
+# primero obtenemos la longitud de la secuecnia mas grande ( L )
+# luego |x| = K*L
+
+
+# se usara el algoritmo de Multiple sequence alignment using swarm intelligence
+# la secuencia alineada puede tener ocmo minimo = max_lenght_seq y como maximo = 2*max_lenght_seq
+# se genera un numero aleatorio que seran la cantidad de gaps para la secuencia ,mas grande, siempre y cuando cumpla con la instruccion anterior
+# luego a las demas secuecnias se agragan gaps, de manera tal que tengan la misma longitud que la secuencia mas grande (instrucciuo anterior)
+
+# el acercamiento es un crossover.
+
+
+
+
+def read_sequences(path):
+    files = glob.glob(path + "*.fasta")
+    max_length = 0
+    min_length = 1000000
+    sequences = []
+    for file in files:
+        seqs = SeqIO.parse(file, "fasta")
+
+        for record in seqs:
+            seq_str = str(record.seq.upper())
+            sequences.append( [ record.id, seq_str ] )
+            if max_length < len( seq_str ):                
+                max_length = len( seq_str )
+            if min_length > len( seq_str ):
+                min_length = len( seq_str )
+
+    sequences = np.array( sequences )
+    k = sequences.shape[0]
+    return sequences, k, max_length, min_length
+
+def read_sequences_s8():
+    max_length = 10
+    min_length = 7
+    sequences = [ 
+        ["s1", "ATGCAAG"], 
+        ["s2", "TAAGTCAAGT"], 
+        ["s3", "ATGCAACT"], 
+        ["s4", "TAAGTCATA"],
+        ["s5", "ATGGATTC"]
+    ]
+    
+    sequences = np.array( sequences )
+    k = sequences.shape[0]
+    return sequences, k, max_length, min_length
+
+
+def sample_fitness(sample):
+    x = sample[0]
+    y = sample[1]
+    #a = sample[2]
+    #b = sample[3]
+    #return (x + 2*y -7)**2 + (2*x + y - 5)**2 + (a + 2*b - 7)**2 + (2*a + b - 5)**2
+    return (x + 2*y -7)**2 + (2*x + y - 5)**2 
+
+def compute_fitness(population):
+    for sample in population:        
+        sample[vector_size] = sample_fitness(sample)
+        
+#sequences, k, max_length, min_length = read_sequences(current_dir + "/seqs/S7/")
+sequences, k, max_length, min_length = read_sequences_s8()
+max_gaps_allowed = math.floor(0.3*max_length)
+gaps_allowed = np.random.randint(max_gaps_allowed+1, size=sequences.shape[0])
+
+print(sequences)
+print(sequences.shape)
+print(k, max_length, min_length)
+print(max_gaps_allowed, gaps_allowed)
+
+
+
+sys.exit(0)
+
+###########################################################################################################
+###########################################################################################################3333
+# Parametros
+population_size = 10
+att_per_seq = int(max_length*0.2)   # gaps dentro de una particula para cada secuencia
+vector_size = k*att_per_seq         # 0.2 porque solo se tiene como maximo un 20% de gaps
+phi_1 = 2
+phi_2 = 2
+iterations = 1
+
+###########################################################################################################
+###########################################################################################################
+print("\n...............................................................")
+print("PARAMETERS:")
+print("population_size:", population_size)
+print("attributes per seq (n) :", att_per_seq)
+print("seq number (k) :", k)
+print("vector_size (n*k) :", vector_size)
+print("rand_1 and rand_2 = [0.1]")
+print("phi_1 and phi_2 = 2")
+print("Iterations:", iterations)
+print("...............................................................\n")
+
+positions = np.random.randint(att_per_seq, size=(population_size, vector_size))
+positions = np.random.randint(2, size=(population_size, vector_size))
+
+print(positions)
+print(positions.shape)
+
+sys.exit(0)
+
+velocities = np.random.uniform(-1, 1, size=(population_size, 3))
+population = np.hstack( (positions, velocities) )
+
+compute_fitness(population)
+
+population = population[population[:, vector_size].argsort()]
+
+best_locals = population[ :, 0:2 ].copy()
+best_locals = np.hstack( ( best_locals, population[ :, vector_size ].reshape( population_size, 1 ).copy() ) )
+
+best_global = best_locals[0]
+
+population_df = pd.DataFrame(data=population, columns=['x', 'y', 'v_x', 'v_y', 'fitness'])
+best_locals_df = pd.DataFrame(data=best_locals, columns=['x', 'y', 'fitness'])
+print("population:\n", population_df)
+print("\nbest locals:\n", best_locals_df)
+print("\nbest global:", best_global)
+
+for iter in range(iterations):
+    print("\n***  ITERATION ", iter, "**************************************************************************")
+    print("********************************************************************************************\n")
+
+    for i in range( population_size ):
+        print( "\nParticle ", i, "=", population[i], "***********************************" )
+        w = random.random()
+        rand_1 = random.random()
+        rand_2 = random.random()
+
+        print("w:", w, "rand 1:", rand_1, "rand 2:", rand_2)
+
+        v_current = population[i, 2:4]
+        p_best = best_locals[i, 0:2]
+        x = population[i, 0:2]
+        g = best_global[0:2]
+        v_next = w*v_current + phi_1 * rand_1 * ( p_best - x ) + phi_2 * rand_2 * ( g - x )
+        x_next = x + v_next
+        new_fitness = sample_fitness( x_next )
+
+        print("new position:", x_next, "new velocity:", v_next, "new fitness:", new_fitness)
+
+        if new_fitness < population[i, 4]:
+            best_locals[i, 0:2] = x_next
+            best_locals[i, 2] = new_fitness
+
+        if new_fitness < best_global[2]:
+            best_global[0:2] = x_next
+            best_global[2] = new_fitness
+
+        population[i, 0:2] = x_next 
+        population[i, 2:4] = v_next
+        population[i, 4] = new_fitness
+
+         
+    population_df = pd.DataFrame(data=population, columns=['x', 'y', 'v_x', 'v_y', 'fitness'])
+    print("\nPopulation:")
+    print(population_df)
+
+    best_locals_df = pd.DataFrame(data=best_locals, columns=['x', 'y', 'fitness'])
+    print("\nbest locals:\n", best_locals_df)
+    print("\nbest global:", best_global)
+
+
+population = population[population[:, vector_size].argsort()]
+population_df = pd.DataFrame(data=population, columns=['x', 'y', 'a', 'b', 'fitness'])
+print("\nLast population:")
+print(population_df)
+
+print("..................................................")
+print("\nRESULT..........................................")
+print("Best fitness:",  best_global[2])
+print("Solution:",  best_global[0:2])
